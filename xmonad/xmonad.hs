64a361551f51c65407700c85f58e6c2cb455ed59
Amol Mandhane <mandhane@google.com>
2016-01-10 03:45:36 +0530

    XMonad paths

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -13,6 +13,8 @@ import XMonad.Layout.Circle
 import XMonad.Layout.Grid
 import qualified XMonad.Layout.Magnifier as Magnifier
 import qualified Data.Map as Map
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ICCCMFocus
 
 main = do
     dzenTop <- spawnPipe myDzenTopBar
@@ -21,19 +23,22 @@ main = do
     conkyBottom <- spawnPipe myConkyBottom
 
     trayer <- spawnPipe myTrayer
-    startup <- spawnPipe "/home/amol/.xmonad/startup.sh"
+    startup <- spawnPipe "~/.xmonad/startup.sh"
 
     xmonad $ ewmh defaultConfig
         { terminal = "gnome-terminal"
         , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
         , layoutHook =  avoidStruts $ (myLayoutHook ||| layoutHook defaultConfig)
-        , logHook = myLogHook dzenTop dzenBottom
+        , logHook = do
+            takeTopFocus
+            myLogHook dzenTop dzenBottom
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         , workspaces = myWorkspaces
         , focusFollowsMouse = False
         , normalBorderColor  = "#888888"
-        , focusedBorderColor = "#0000ff"
-        , borderWidth = 3
+        , focusedBorderColor = "#008888"
+        , borderWidth = 1
+        , startupHook = setWMName "LG3D"
         } `additionalKeysP` myKeys
 
 
@@ -51,23 +56,23 @@ myTrayer = "trayer --edge top --align right --SetDockType true --SetPartialStrut
 
 
 myDzenStyle  = " -e 'button2=;' -h '16' -fg '" ++ fgGray ++ "' -bg '" ++ darkBlack ++ "' -fn '" ++ dzenFont 10 ++ "'"
-myDzenTopBar = "dzen2 -w '850' -ta 'l'" ++ myDzenStyle
-myDzenBottomBar = "dzen2 -y '752' -w '400' -ta 'l'" ++ myDzenStyle
+myDzenTopBar = "dzen2 -w '1240' -ta 'l'" ++ myDzenStyle
+myDzenBottomBar = "dzen2 -y '1184' -w '400' -ta 'l'" ++ myDzenStyle
 
-myConkyTop = "conky -c ~/.xmonad/conky_utilities/conky_top_rc | dzen2 -x '850' -w '366' -ta 'r'" ++ myDzenStyle
-myConkyBottom = "conky -c ~/.xmonad/conky_utilities/conky_bottom_rc | dzen2 -y '752' -x '400' -w '966' -ta 'r'" ++ myDzenStyle
--- myDzenConky  = "conky -c ~/.xmonad/conkyrc | dzen2 -x '656' -w '560' -ta 'r'" ++ myDzenStyle
+myConkyTop = "conky -c ~/.xmonad/conky_utilities/conky_top_rc | dzen2 -x '1240' -w '470' -ta 'r'" ++ myDzenStyle
+myConkyBottom = "conky -c ~/.xmonad/conky_utilities/conky_bottom_rc | dzen2 -y '1184' -x '400' -w '1520' -ta 'r'" ++ myDzenStyle
+-- myDzenConky  = "conky -c /usr/local/google/home/mandhane/.xmonad/conkyrc | dzen2 -x '656' -w '560' -ta 'r'" ++ myDzenStyle
 
 myWorkspaces_img = [
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/web.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/term.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/dev.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/main.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/media.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/browse.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/apps.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/misc.xbm)",
-     "^i(/home/amol/.xmonad/dzen2_icons/workspaces/extra.xbm)"]
+     "^i(.xmonad/dzen2_icons/workspaces/web.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/term.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/dev.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/main.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/media.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/browse.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/apps.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/misc.xbm)",
+     "^i(.xmonad/dzen2_icons/workspaces/extra.xbm)"]
 
 
 myWorkspaces = ["^ca(1, xdotool key super+" ++ (show i) ++ ")" ++ s ++ "^ca()" | (s, i) <- zip myWorkspaces_img [1..9]]
@@ -75,6 +80,7 @@ myWorkspaces = ["^ca(1, xdotool key super+" ++ (show i) ++ ")" ++ s ++ "^ca()" |
 
 myManageHook = composeAll . concat $
    [ [ className =? "Firefox"             --> doShift (myWorkspaces !! 0) ]
+    , [ className =? "Google-chrome"      --> doShift (myWorkspaces !! 0) ]
     , [ className =? "Sublime_text"       --> doShift (myWorkspaces !! 2) ]
     , [ className =? "Gvim"               --> doShift (myWorkspaces !! 2) ]
     , [ className =? "Vlc"                --> doShift (myWorkspaces !! 4) ]
@@ -118,7 +124,7 @@ myLogHook top bottom = do
 myKeys = [("M1-<Tab>"   , cycleRecentWindows [xK_Alt_L] xK_Tab xK_Tab ) -- classic alt-tab behaviour
          , ("M-<Return>" , spawn "dmenu_run -b" ) -- app launcher
          , ("M-e"        , spawn "nemo --no-desktop" ) -- launch file manager
-         , ("C-M1-l"     , spawn "/home/amol/.xmonad/lock.sh" ) -- lock screen
+         , ("C-M1-l"     , spawn "~/.xmonad/lock.sh" ) -- lock screen
          , ("M-S--", sendMessage Magnifier.MagnifyLess )
          , ("M-S-=", sendMessage Magnifier.MagnifyMore )
          ]
