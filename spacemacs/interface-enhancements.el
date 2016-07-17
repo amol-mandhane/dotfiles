9bf915a2a8290ba3eb0221f3297d9761f7739028
Amol Mandhane <mandhane@google.com>
2016-07-18 01:09:49 +0530

    Revert Java completion back to company.

--- spacemacs/interface-enhancements.el
+++ spacemacs/interface-enhancements.el
@@ -11,24 +11,4 @@
 (setq helm-mode-fuzzy-match t)
 (setq helm-completion-in-region-fuzzy-match t)
 
-;; Make sure FCI doesn't mess up auto-completion
-(defun config/fci-enabled-p () (symbol-value 'fci-mode))
-
-(defvar config/fci-mode-suppressed nil)
-(make-variable-buffer-local 'config/fci-mode-suppressed)
-
-(defadvice popup-create (before suppress-fci-mode activate)
-  "Suspend fci-mode while popups are visible"
-  (let ((fci-enabled (config/fci-enabled-p)))
-    (when fci-enabled
-      (setq config/fci-mode-suppressed fci-enabled)
-      (turn-off-fci-mode))))
-
-(defadvice popup-delete (after restore-fci-mode activate)
-  "Restore fci-mode when all popups have closed"
-  (when (and config/fci-mode-suppressed
-             (null popup-instances))
-    (setq config/fci-mode-suppressed nil)
-    (turn-on-fci-mode)))
-
 (provide 'interface-enhancements)
--- spacemacs/lang-config.el
+++ spacemacs/lang-config.el
@@ -2,10 +2,4 @@
 ; Java has big words. Use 100 character limit.
 (add-hook 'java-mode-hook (lambda () (setq fill-column 100)))
 
-;; company with eclim sucks. Use auto-complete.
-(add-hook 'java-mode-hook (lambda () (if (bound-and-true-p company-mode) (company-mode -1))) t)
-(add-hook 'java-mode-hook 'auto-complete-mode)
-(add-to-list 'ac-modes 'java-mode)
-
-
 (provide 'lang-config)
