99377df014244da9273bee245cb801aa64d14efd
Amol Mandhane <mandhane@google.com>
2016-10-07 23:22:50 +0530

    Upgrade to spacemacs v0.200.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -1,21 +1,16 @@
 ;; -*- mode: emacs-lisp -*-
-;; This file is loaded by Spacemacs at startup.
-;; It must be stored in your home directory.
 
 (setq user-full-name "Amol Mandhane"
       user-mail-address "amol.mandhane@gmail.com")
 
 (defun dotspacemacs/layers ()
   (setq-default
-   dotspacemacs-distribution 'spacemacs
-   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers/")
+   dotspacemacs-distribution 'spacemacs-base
+   dotspacemacs-enable-lazy-installation 'unused
+   dotspacemacs-ask-for-lazy-installation t
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers")
    dotspacemacs-configuration-layers
    '(
-     ;; ----------------------------------------------------------------
-     ;; Example of useful layers you may want to use right away.
-     ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
-     ;; <M-m f e R> (Emacs style) to install them.
-     ;; ----------------------------------------------------------------
      (auto-completion :variables
                       auto-completion-enable-help-tooltip t
                       auto-completion-enable-snippets-in-popup t)
@@ -29,6 +24,7 @@
      go
      gtags
      haskell
+     helm
      html
      ipython-notebook
      java
@@ -40,10 +36,22 @@
      protobuf
      python
      semantic
-     shell
+     (shell :variables
+            shell-default-height 30
+            shell-default-position 'bottom)
      shell-scripts
+     spacemacs-completion
+     spacemacs-editing
+     spacemacs-editing-visual
+     spacemacs-evil
+     spacemacs-language
+     spacemacs-layouts
+     spacemacs-misc
+     spacemacs-org
+     spacemacs-ui
+     spacemacs-ui-visual
      spell-checking
-     syntax-checking
+     (syntax-checking :variables syntax-checking-enable-tooltips nil)
      themes-megapack
      version-control
      xkcd
@@ -51,8 +59,9 @@
      ycmd
      )
    dotspacemacs-additional-packages '(litable)
+   dotspacemacs-frozen-packages '()
    dotspacemacs-excluded-packages '()
-   dotspacemacs-delete-orphan-packages t))
+   dotspacemacs-install-packages 'used-only))
 
 (defun dotspacemacs/init ()
   "Initialization function.
@@ -63,21 +72,19 @@ values."
   (setq-default
    dotspacemacs-elpa-https t
    dotspacemacs-elpa-timeout 5
-   dotspacemacs-check-for-update t
+   dotspacemacs-check-for-update nil
+   dotspacemacs-elpa-subdirectory nil
    dotspacemacs-editing-style 'hybrid
    dotspacemacs-verbose-loading nil
    dotspacemacs-startup-banner nil
-   dotspacemacs-startup-lists '(recents projects)
-   dotspacemacs-startup-recent-list-size 5
+   dotspacemacs-startup-lists '((recents . 5)
+                                (projects . 7))
+   dotspacemacs-startup-buffer-responsive t
    dotspacemacs-scratch-mode 'org-mode
    dotspacemacs-themes '(flatland
-                         spacemacs-light
                          spacemacs-dark
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         zenburn
-                         monokai)
+                         spacemacs-light
+                         leuven)
    dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Inconsolata"
                                :size 18
@@ -88,19 +95,23 @@ values."
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   dotspacemacs-emacs-command-key "SPC"
    dotspacemacs-distinguish-gui-tab nil
-   dotspacemacs-command-key ":"
    dotspacemacs-remap-Y-to-y$ t
+   dotspacemacs-retain-visual-state-on-shift t
+   dotspacemacs-visual-line-move-text nil
+   dotspacemacs-ex-substitute-global nil
    dotspacemacs-default-layout-name "Default"
-   dotspacemacs-display-default-layout nil
+   dotspacemacs-display-default-layout t
    dotspacemacs-auto-resume-layouts nil
-   dotspacemacs-auto-save-file-location nil
+   dotspacemacs-large-file-size 1
+   dotspacemacs-auto-save-file-location 'cache
    dotspacemacs-max-rollback-slots 5
-   dotspacemacs-use-ido nil
    dotspacemacs-helm-resize nil
    dotspacemacs-helm-no-header nil
    dotspacemacs-helm-position 'bottom
-   dotspacemacs-enable-paste-micro-state nil
+   dotspacemacs-helm-use-fuzzy 'always
+   dotspacemacs-enable-paste-transient-state nil
    dotspacemacs-which-key-delay 0.4
    dotspacemacs-which-key-position 'bottom
    dotspacemacs-loading-progress-bar t
@@ -108,26 +119,21 @@ values."
    dotspacemacs-fullscreen-use-non-native nil
    dotspacemacs-maximized-at-startup t
    dotspacemacs-active-transparency 90
-   dotspacemacs-inactive-transparency 90
+   dotspacemacs-inactive-transparency 80
+   dotspacemacs-show-transient-state-title t
+   dotspacemacs-show-transient-state-color-guide t
    dotspacemacs-mode-line-unicode-symbols nil
    dotspacemacs-smooth-scrolling t
    dotspacemacs-line-numbers 'relative
+   dotspacemacs-folding-method 'evil
    dotspacemacs-smartparens-strict-mode t
+   dotspacemacs-smart-closing-parenthesis nil
    dotspacemacs-highlight-delimiters 'all
    dotspacemacs-persistent-server nil
    dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
    dotspacemacs-default-package-repository nil
    dotspacemacs-whitespace-cleanup 'trailing
    ))
-
-(defun dotspacemacs/user-init ()
-  "Initialization function for user code.
-It is called immediately after `dotspacemacs/init', before layer configuration
-executes.
- This function is mostly useful for variables that need to be set
-before packages are loaded. If you are unsure, you should try in setting them in
-`dotspacemacs/user-config' first."
-  )
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
@@ -136,7 +142,6 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-
   (require 'ob-tangle)
   (org-babel-load-file (expand-file-name "~/.spacemacs.d/user-config.org"))
 
