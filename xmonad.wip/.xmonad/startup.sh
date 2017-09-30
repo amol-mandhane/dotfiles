c684918cf94c78acbdc64cb7ddd6292ece4603f3
Amol Mandhane <amol.mandhane@gmail.com>
2017-09-30 00:09:49 -0700

    Startup tasks change for laptop.

--- xmonad.wip/.xmonad/startup.sh
+++ xmonad.wip/.xmonad/startup.sh
@@ -5,9 +5,9 @@ if [ ! $(pgrep -x "nm-applet") > /dev/null ]; then
   nm-applet &
 fi
 
-if [ -x "$(command -v mate-settings-daemon)" ]; then
-  mate-settings-daemon &
-fi
+# if [ -x "$(command -v mate-settings-daemon)" ]; then
+#   mate-settings-daemon &
+# fi
 
 if [ -f $HOME/.xsettingsd ]; then
   xsettingsd &
@@ -17,3 +17,15 @@ fi
 setxkbmap -option ctrl:swapcaps
 # Disable Caps_Lock
 xmodmap -e "keysym Caps_Lock = NoSymbol"
+
+
+if [ -x "$(command -v synclient)" ]; then
+  synclient AreaBottomEdge=1600
+  synclient AreaTopEdge=400
+  synclient AreaLeftEdge=700
+  synclient AreaRightEdge=3350
+
+  xinput --set-prop 14 "Synaptics Finger" 25 60 0
+
+  xbacklight -set 25
+fi
